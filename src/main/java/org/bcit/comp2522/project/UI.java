29834f9f6912f8ec7a2a2475aba9a29d4d90ad33
Amarjot <amarjot.sangha28@gmail.com>
2023-03-11 21:59:14 -0800

    Created a Start,Choose your character,Pause,and End menus.

--- src/main/java/org/bcit/comp2522/project/UI.java
+++ src/main/java/org/bcit/comp2522/project/UI.java
@@ -1,6 +1,6 @@
 package org.bcit.comp2522.project;
 
-public class UI {
+public class UI extends Window{
   private int XPos;
   private int YPos;
 
@@ -11,6 +11,17 @@ public class UI {
 
   private void drawUI() {
     //TODO: Implement this method
+    background(0);
+    //welcome message
+    textSize(100);
+    fill(0, 408, 612);
+
+    //button
+    fill(153);
+    rect(120, 200, 200, 50);
+    textSize(50);
+    fill(200, 0, 0);
+    text("Welcome!", 140, 245);
   }
 
   public int getXPos() {
