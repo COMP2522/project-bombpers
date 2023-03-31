a802649facb535630102af56b3986ef0e29da693
bli129 <bennyli.byli@gmail.com>
2023-03-31 02:52:10 -0700

    Changed dimension variables to constants and brightened font

--- src/main/java/org/bcit/comp2522/project/HPDisplay.java
+++ src/main/java/org/bcit/comp2522/project/HPDisplay.java
@@ -5,23 +5,23 @@ import processing.core.PConstants;
 import processing.core.PVector;
 
 public class HPDisplay {
+  private static final int X_POS = 70;
+  private static final int Y_POS = Window.WINDOW_HEIGHT - 50;
   private PApplet pApp;
   private Player player;
   private PVector position;
   private int health;
-  private int xpos = 70;
-  private int ypos = Window.WINDOW_HEIGHT - 50;
 
   public HPDisplay(PApplet p, CollectionManager collectionManager) {
     this.pApp = p;
     this.player = (Player) collectionManager.getPlayer();
-    this.position = new PVector(xpos, ypos);
+    this.position = new PVector(X_POS, Y_POS);
     this.health = player.getHealth();
   }
 
   public void draw() {
     pApp.textAlign(PConstants.RIGHT, PConstants.CENTER);
-    pApp.fill(200, 200, 200);
+    pApp.fill(220, 220, 220);
     pApp.textSize(24);
     pApp.text("HP: " + health, position.x, position.y);
   }
