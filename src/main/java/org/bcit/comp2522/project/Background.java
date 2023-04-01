7ae2a2c4a11f70c07f3256443ddb00484fae7fa1
ssidhu271 <113222769+ssidhu271@users.noreply.github.com>
2023-04-01 15:51:44 -0700

    Made a new background for the game and uploaded that and fixed where the projectile was from (#203)

--- src/main/java/org/bcit/comp2522/project/Background.java
+++ src/main/java/org/bcit/comp2522/project/Background.java
@@ -7,20 +7,68 @@ import processing.core.PImage;
  * Background class.
  */
 public class Background implements Drawable {
+
+  /**
+   * The parent PApplet.
+   */
   private final PApplet parent;
+
+  /**
+   * The background image.
+   */
   private final PImage backgroundImage;
 
+  /**
+   * The offset of the background image.
+   */
+  private float offset;
+
+  /**
+   * The speed of the background image.
+   */
+  private final float speed;
+
+  /**
+   * The speed of the background image.
+   */
+  private static final float SPEED_OF_SCROLL = 0.5f;
+
+  /**
+   * The starting point of the background image.
+   */
+  private static final float Y_POS_STARTING_POINT = 0;
+
+  /**
+   * Constructor for the Background class.
+   * @param parent the parent PApplet
+   */
   public Background(PApplet parent) {
     this.parent = parent;
     this.backgroundImage = loadBackgroundImage();
+    this.offset = 0;
+    this.speed = SPEED_OF_SCROLL;
   }
 
+  /**
+   * Loads the background image.
+   * @return the background image
+   */
   private PImage loadBackgroundImage() {
-    return parent.loadImage("../img/bg3.png");
+    return parent.loadImage("../img/grass_bgv4.png");
   }
 
+  /**
+   * Draws the background image.
+   */
   @Override
   public void draw() {
-    parent.image(backgroundImage, 0, 0, parent.width, parent.height);
+    float imageWidth = (float) (backgroundImage.width * parent.height) / backgroundImage.height;
+    parent.image(backgroundImage, offset, Y_POS_STARTING_POINT, imageWidth, parent.height);
+    parent.image(backgroundImage, offset + imageWidth, Y_POS_STARTING_POINT, imageWidth, parent.height);
+
+    offset -= speed;
+    if (offset <= -imageWidth) {
+      offset = Y_POS_STARTING_POINT;
+    }
   }
 }
